5630c868a636d23bbfa0b73cc45813e0d8e2b7cc
Irene Xing <irenexing0220@gmail.com>
2022-10-28 16:47:05 -0400

    first selection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import "./App.scss";
 import data from './interchanges.json';
 
 function TollCalculator() {
+  const locations = data.locations
+  console.log  (locations)
+ const name = [];
+ //iterate through array
+ for (const i of Object.keys(locations)) {name.push (locations[i].name)}
+ console.log (Object.keys(locations))
+ console.log (name)
+  
   return (
     <div className="toll">
       <header>TOLL CALCULATOR</header>
@@ -10,14 +18,14 @@ function TollCalculator() {
         aria-label=".form-select-lg example"
       >
         <option selected>Open this select menu</option>
-        <option value="1">One</option>
+      {name.map ((location) => {return <option>{location}</option>})}
       </select>
       <select
         class="form-select form-select-lg mb-3"
         aria-label=".form-select-lg example"
       >
         <option selected>Open this select menu</option>
-        <option value="1">One</option>
+      {name.map ((location) => {return <option>{location}</option>})}
       </select>
      <button>Calculate</button>
      <p></p>
