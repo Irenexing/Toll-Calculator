e2a5f487b53845b7eb2b585577acacdffa23d803
Irene Xing <irenexing0220@gmail.com>
2022-10-31 11:26:59 -0400

    temp save

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.scss";
-import data from './interchanges.json';
+import data from "./interchanges.json";
 
 function TollCalculator() {
-  const locations = data.locations
-  console.log  (locations)
- const name = [];
- //iterate through array
- for (const i of Object.keys(locations)) {name.push (locations[i].name)}
- console.log (Object.keys(locations))
- console.log (name)
-  
+  const locations = data.locations;
+  console.log(locations);
+  const arrayNumber = Object.keys(locations);
+  const departureName = [];
+  //iterate through array
+  for (const i of arrayNumber) {
+    departureName.push(locations[i].name);
+  }
+  console.log(Object.keys(locations));
+  console.log(arrayNumber)
+  console.log(departureName);
+
+  const destinationId = [];
+  for (const i of arrayNumber) {
+    destinationId.push(locations[i].routes[i].toId);
+  }
+  console.log(locations[1].routes[0].toId)
+  console.log(destinationId)
+
   return (
     <div className="toll">
       <header>TOLL CALCULATOR</header>
@@ -18,17 +29,21 @@ function TollCalculator() {
         aria-label=".form-select-lg example"
       >
         <option selected>Open this select menu</option>
-      {name.map ((location) => {return <option>{location}</option>})}
+        {departureName.map((location) => {
+          return <option>{location}</option>;
+        })}
       </select>
       <select
         class="form-select form-select-lg mb-3"
         aria-label=".form-select-lg example"
       >
         <option selected>Open this select menu</option>
-      {name.map ((location) => {return <option>{location}</option>})}
+        {departureName.map((location) => {
+          return <option>{location}</option>;
+        })}
       </select>
-     <button>Calculate</button>
-     <p></p>
+      <button>Calculate</button>
+      <p></p>
     </div>
   );
 }
