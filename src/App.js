f6fc7b77b4b9a971e8efac71880975f3e1d34fa7
Irene Xing <irenexing0220@gmail.com>
2022-10-31 16:14:25 -0400

    storing locations

--- src/App.js
+++ src/App.js
@@ -1,48 +1,80 @@
 import "./App.scss";
-import data from "./interchanges.json";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function TollCalculator() {
-  const locations = data.locations;
-  console.log(locations);
+  const [location, setLocation] = useState({});
+  const [optionA, setOptionA] = useState("Point A");
+  const [optionB, setOptionB] = useState("Point B");
+  const [locationA, setLocationA] = useState("");
+  const [locationB, setLocationB] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/`)
+      .then((res) => {
+        setLocation(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (location === null) {
+    return <main className="loading">Loading...</main>;
+  }
+
+  const locations = location.locations;
   const arrayNumber = Object.keys(locations);
-  const departureName = [];
+  const route = [];
   //iterate through array
   for (const i of arrayNumber) {
-    departureName.push(locations[i].name);
+    route.push(locations[i]);
   }
-  console.log(Object.keys(locations));
-  console.log(arrayNumber)
-  console.log(departureName);
 
-  const destinationId = [];
-  for (const i of arrayNumber) {
-    destinationId.push(locations[i].routes[i].toId);
+  route.forEach((route, i) => {
+    route.id = i+1;
+  })
+
+  const handleOptionA =(e) => {
+    setOptionA(e.target.value)
   }
-  console.log(locations[1].routes[0].toId)
-  console.log(destinationId)
+  const handleOptionB =(e) => {
+    setOptionB(e.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("hi");
+  };
+
+  const pointA =route.find((route) => {
+    return route.name === optionA;
+  })
+
+  const pointB =route.find((route) => {
+    return route.name === optionB;
+  })
+  console.log (pointA, pointB)
 
   return (
     <div className="toll">
       <header>TOLL CALCULATOR</header>
-      <select
-        class="form-select form-select-lg mb-3"
-        aria-label=".form-select-lg example"
-      >
-        <option selected>Open this select menu</option>
-        {departureName.map((location) => {
-          return <option>{location}</option>;
-        })}
-      </select>
-      <select
-        class="form-select form-select-lg mb-3"
-        aria-label=".form-select-lg example"
-      >
-        <option selected>Open this select menu</option>
-        {departureName.map((location) => {
-          return <option>{location}</option>;
-        })}
-      </select>
-      <button>Calculate</button>
+      <form onSubmit={handleSubmit}>
+        <select>
+          <option>Open this select menu</option>
+          {route.map((pointA) => {
+            return <option value={arrayNumber} onClick ={handleOptionA}>{pointA}</option>;
+          })}
+        </select>
+        <select>
+          <option>Open this select menu</option>
+          {route.map((pointB) => {
+            return <option value={arrayNumber} onClick = {handleOptionB}>{pointB}</option>;
+          })}
+        </select>
+        <button>Calculate</button>
+      </form>
       <p></p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import TollCalculator from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <TollCalculator />
-  </React.StrictMode>
+
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
