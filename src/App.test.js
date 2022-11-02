7c3a5c56c60548800c1b1d90f7fd1f69e49ab940
Irene Xing <irenexing0220@gmail.com>
2022-11-01 22:53:45 -0400

    test2

--- src/App.test.js
+++ src/App.test.js
@@ -74,77 +74,77 @@ describe("Distance", () => {
   });
 });
 
-// describe("Distance", () => {
-//     const route = [
-//       {
-//         name: "QEW",
-//         id: 1,
-//         routes: [
-//           {
-//             toId: 2,
-//             distance: 6.062,
-//           },
-//         ],
-//       },
-//       {
-//         name: "Dundas Street",
-//         id: 2,
-//         routes: [
-//           {
-//             toId: 3,
-//             distance: 3.847,
-//           },
-//           {
-//             toId: 1,
-//             distance: 6.062,
-//           },
-//         ],
-//       },
-//       {
-//         name: "Appleby Line",
-//         id: 3,
-//         routes: [
-//           {
-//             toId: 4,
-//             distance: 4.153,
-//           },
-//           {
-//             toId: 2,
-//             distance: 3.847,
-//           },
-//         ],
-//       },
-//       {
-//         name: "Bronte Road",
-//         id: 4,
-//         routes: [
-//           {
-//             toId: 5,
-//             distance: 4.927,
-//           },
-//           {
-//             toId: 3,
-//             distance: 4.153,
-//           },
-//         ],
-//       },
-//       {
-//         name: "Neyagawa Blvd.",
-//         id: 5,
-//         routes: [
-//           {
-//             toId: 6,
-//             distance: 3.227,
-//           },
-//           {
-//             toId: 4,
-//             distance: 4.927,
-//           },
-//         ],
-//       },
-//     ];
-//     it("should calculate the correct distance", () => {
-//       expect(calculateDistance(route,route[4], route[0])).toBe(18.989);
-//     });
-//   });
+describe("Distance", () => {
+    const route = [
+      {
+        name: "QEW",
+        id: 1,
+        routes: [
+          {
+            toId: 2,
+            distance: 6.062,
+          },
+        ],
+      },
+      {
+        name: "Dundas Street",
+        id: 2,
+        routes: [
+          {
+            toId: 3,
+            distance: 3.847,
+          },
+          {
+            toId: 1,
+            distance: 6.062,
+          },
+        ],
+      },
+      {
+        name: "Appleby Line",
+        id: 3,
+        routes: [
+          {
+            toId: 4,
+            distance: 4.153,
+          },
+          {
+            toId: 2,
+            distance: 3.847,
+          },
+        ],
+      },
+      {
+        name: "Bronte Road",
+        id: 4,
+        routes: [
+          {
+            toId: 5,
+            distance: 4.927,
+          },
+          {
+            toId: 3,
+            distance: 4.153,
+          },
+        ],
+      },
+      {
+        name: "Neyagawa Blvd.",
+        id: 5,
+        routes: [
+          {
+            toId: 6,
+            distance: 3.227,
+          },
+          {
+            toId: 4,
+            distance: 4.927,
+          },
+        ],
+      },
+    ];
+    it("should calculate the correct distance", () => {
+      expect(calculateDistance(route,route[4], route[0])).toBe(18.989);
+    });
+  });
   
